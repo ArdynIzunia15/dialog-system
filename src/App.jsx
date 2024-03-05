56a05e0399c97b5c8834fc7caf4d1df5690f0427
ArdynIzunia15 <navs150103@gmail.com>
2024-03-05 21:59:15 +0700

    Fix media query from local to cloud

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import "bootstrap/dist/css/bootstrap.css";
 import { useState } from "react";
 import { ReactTyped } from "react-typed";
 
+// Harus coba imagenya jadiin cloud
+
 export default function App() {
   const dialogs = [
     "Ah udahAh udahAh udahAh udahAh udahAh udahAh udahAh udahAh udahAh udahAh udahAh udahAh udahAh udahAh udah",
     "Sekarang ini dialog 2",
     "Selanjutnya ini dialog 3",
     "Dah beres bang",
+  ];
+  let imgs = [
+    "https://github.com/ArdynIzunia15/dialog-system/blob/main/src/assets/Terrain.gif?raw=true",
+    "https://github.com/ArdynIzunia15/dialog-system/blob/main/src/assets/slime-11.gif?raw=true",
+    "https://github.com/ArdynIzunia15/dialog-system/blob/main/src/assets/speech_bubble_top.png?raw=true",
+    "https://github.com/ArdynIzunia15/dialog-system/blob/main/src/assets/speech_bubble_bottom.png?raw=true",
+    "https://github.com/ArdynIzunia15/dialog-system/blob/main/src/assets/ChatEmote.gif?raw=true",
   ];
   const [dialogIndex, setDialogIndex] = useState(0);
   const [btnDisabled, setBtnDisabled] = useState(false);
@@ -29,24 +38,28 @@ export default function App() {
     setChatBubbleVisible("invisible");
   };
 
+  const handleBackgroundOnLoad = () => {
+    alert("Beres Bang");
+  };
+
   return (
     <>
       {/* Background Components */}
       <div className="altar-container">
-        <img src="src/assets/Terrain.gif" className="background-image" />
-        <img src="src/assets/slime-11.gif" className="slime img-fluid" />
         <img
-          src="src/assets/ChatEmote.gif"
-          className={chatBubbleVisible}
-          id="chat-bubble"
+          src={imgs[0]}
+          className="background-image"
+          onLoad={handleBackgroundOnLoad}
         />
+        <img src={imgs[1]} className="slime img-fluid" />
+        <img src={imgs[4]} className={chatBubbleVisible} id="chat-bubble" />
       </div>
 
       {/* End of Background Components*/}
       <div className="container-fluid overlay-content">
         {/* Animated Text Components */}
         <div className="row">
-          <img className="img-fluid" src="/src/assets/speech_bubble_top.png" />
+          <img className="img-fluid" src={imgs[2]} />
         </div>
         <div className="row cs-container-dialog dialog-custom-margin animated-text-background">
           <ReactTyped
@@ -68,10 +81,7 @@ export default function App() {
           </button>
         </div>
         <div className="row">
-          <img
-            className="img-fluid"
-            src="/src/assets/speech_bubble_bottom.png"
-          />
+          <img className="img-fluid" src={imgs[3]} />
         </div>
       </div>
     </>
