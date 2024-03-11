00fd2ffd72adcf9c4bca8c5287278eea8cd1e2ea
ArdynIzunia15 <navs150103@gmail.com>
2024-03-11 19:15:12 +0700

    Change media queries to local instead of cloud

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,9 @@ import { useState } from "react";
 import { ReactTyped } from "react-typed";
 import useSound from "use-sound";
 
-// Harus coba imagenya jadiin cloud
+// Global Variables
 
 export default function App() {
-  const randomNumberInRange = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
   const dialogs = [
     "Loading...",
     "Click start to begin...",
@@ -17,13 +13,6 @@ export default function App() {
     "Sekarang ini dialog 2",
     "Selanjutnya ini dialog 3",
     "Dah beres bang",
-  ];
-  let imgs = [
-    "https://github.com/ArdynIzunia15/dialog-system/blob/main/src/assets/Terrain.gif?raw=true",
-    "https://github.com/ArdynIzunia15/dialog-system/blob/main/src/assets/slime-11.gif?raw=true",
-    "https://github.com/ArdynIzunia15/dialog-system/blob/main/src/assets/speech_bubble_top.png?raw=true",
-    "https://github.com/ArdynIzunia15/dialog-system/blob/main/src/assets/speech_bubble_bottom.png?raw=true",
-    "https://github.com/ArdynIzunia15/dialog-system/blob/main/src/assets/ChatEmote.gif?raw=true",
   ];
   const [dialogIndex, setDialogIndex] = useState(0);
   const [btnDisabled, setBtnDisabled] = useState(true);
@@ -37,7 +26,6 @@ export default function App() {
   const handleClickNext = () => {
     // Play Sound
     playClick();
-    // /sounds/Click1.WAV
     if (dialogIndex == 1) {
       setMainBtnText("NEXT");
       setMainBtnType("btn btn-primary m-2 justify-content-end");
@@ -76,19 +64,23 @@ export default function App() {
       {/* Background Components */}
       <div className="altar-container">
         <img
-          src={imgs[0]}
+          src="/media/Terrain.gif"
           className="background-image"
           onLoad={handleBackgroundOnLoad}
         />
-        <img src={imgs[1]} className="slime img-fluid" />
-        <img src={imgs[4]} className={chatBubbleVisible} id="chat-bubble" />
+        <img src="/media/slime-11.gif" className="slime img-fluid" />
+        <img
+          src="/media/ChatEmote.gif"
+          className={chatBubbleVisible}
+          id="chat-bubble"
+        />
       </div>
 
       {/* End of Background Components*/}
       <div className="container-fluid overlay-content">
         {/* Animated Text Components */}
         <div className="row">
-          <img className="img-fluid" src={imgs[2]} />
+          <img className="img-fluid" src="/media/speech_bubble_top.png" />
         </div>
         <div className="row cs-container-dialog dialog-custom-margin animated-text-background">
           <ReactTyped
@@ -110,7 +102,7 @@ export default function App() {
           </button>
         </div>
         <div className="row">
-          <img className="img-fluid" src={imgs[3]} />
+          <img className="img-fluid" src="/media/speech_bubble_bottom.png" />
         </div>
       </div>
     </>
